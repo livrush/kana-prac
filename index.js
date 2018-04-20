c6b2790e2660284ceaa09ef4cfaf65876e5a8e4a
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-19 22:25:06 -0500

    add simple implementation of random katakana -> romaji

---
+++ index.js
@@ -0,0 +1,16 @@
+const gimei = require('node-gimei');
+const kuroshiro = require("kuroshiro");
+
+const address = gimei.address();
+const addressKatakana = address.katakana();
+
+console.log(addressKatakana);
+
+const splitAddress = addressKatakana.split('');
+
+kuroshiro.init((err) => {
+  var romajiSyllables = splitAddress.map(kana => {
+    return kuroshiro.convert(kana, { to: 'romaji' });
+  });
+  console.log(romajiSyllables.join(' '));
+});
