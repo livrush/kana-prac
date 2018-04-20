609efc0da37e1b5be00eb4b66741c6f7db0c1eda
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-20 11:38:13 -0500

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  }
+  },
+  resolve: {
+    extensions: ['.js'],
+    alias: {
+        'fs': 'memfs',
+    }
+  },
+  // node: {
+  //   fs: 'mock',
+  // },
+  // target: 'web',
+  // target: "webworker", // or 'node' or 'node-webkit'
+  // externals: {
+  //     fs:    "commonjs fs",
+  //     path:  "commonjs path"
+  // },
 };
