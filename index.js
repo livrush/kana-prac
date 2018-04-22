029cedcf559aaaf4724c1109de97ecf88b9905f4
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-22 17:43:26 -0500

    begin using jquery to adjust colors/etc

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-const address = gimei.address();
-const addressKatakana = address.katakana();
+$(document).ready(function() {
+  console.log('ready');
+  const name = gimei.name();
+  const nameKatakana = name.katakana();
+  const colors = pafiumeColors.scheme(2);
+  const colorDark = colors[0].hues[3];
+  const colorLight = colors[0].hues[1];
+  $('.practice').toggle();
+  $('body')
+    .css({
+      background: colorLight,
+      color: colorLight,
+    });
+  $('.spinner')
+    .css({
+      color: colorDark,
+    });
+  $('#practice-text')
+    .text(nameKatakana);
 
-console.log(addressKatakana);
 
-const splitAddress = addressKatakana.split('');
-
-kuroshiro.init((err) => {
-  var romajiSyllables = splitAddress.map(kana => {
-    return kuroshiro.convert(kana, { to: 'romaji' });
-  });
-  console.log(romajiSyllables.join(' '));
+  kuroshiro.init((err) => {
+    const splitName = nameKatakana.split('');
+    var romajiSyllables = splitName.map(kana => {
+      return kuroshiro.convert(kana, { to: 'romaji' });
+    });
+    // console.log(romajiSyllables.join(' '));
+    $('.spinner').toggle();
+    $('.practice').toggle();
+    $('.background-color')
+      .css('background-color', colorDark)
+      .addClass('slide-open');
+    createTextInput(colorDark);
+  })
 });
+
+function createTextInput(color) {
+  const $practiceInput = $('<input>')
+    .css('color', color)
+    .attr('placeholder', 'Practice Here')
+    .appendTo('.practice')
+    .focus();
+  console.log($practiceInput);
+}
