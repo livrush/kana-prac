edc0a2811fccec61f9ff9983e567da63e8b51385
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-22 20:59:00 -0500

    update classes begin implementing more features

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ $(document).ready(function() {
   console.log('ready');
   const name = gimei.name();
   const nameKatakana = name.katakana();
-  const colors = pafiumeColors.scheme(2);
-  const colorDark = colors[0].hues[3];
-  const colorLight = colors[0].hues[1];
-  $('.practice').toggle();
+  const colors = pafiumeColors.random();
+  const colorDark = colors.hues[3];
+  const colorLight = colors.hues[1];
+  $('#user-input').toggle();
   $('body')
     .css({
       background: colorLight,
       color: colorLight,
+    });
+  $('.bottom')
+    .css({
+      color: colorDark,
     });
   $('.spinner')
     .css({
@@ -25,10 +29,9 @@ $(document).ready(function() {
     var romajiSyllables = splitName.map(kana => {
       return kuroshiro.convert(kana, { to: 'romaji' });
     });
-    // console.log(romajiSyllables.join(' '));
     $('.spinner').toggle();
-    $('.practice').toggle();
-    $('.background-color')
+    $('#user-input').toggle();
+    $('.top')
       .css('background-color', colorDark)
       .addClass('slide-open');
     createTextInput(colorDark, romajiName);
@@ -36,23 +39,47 @@ $(document).ready(function() {
 });
 
 function createTextInput(color, romajiName) {
-  const failures = 0;
+  let failures = 0;
   const $practiceInput = $('<input>')
-    .css('color', color)
-    .attr('placeholder', 'Practice Here')
+    .addClass('user-input text-med')
+    .css({
+      color: color,
+      'border-bottom': `${color} 3px solid`,
+    })
+    // .attr('placeholder', 'Practice Here')
     .keyup((event) => {
       const $event = $(event.currentTarget);
       if (event.keyCode === 13) {
         const userInput = $event.val();
-        if (userInput.toLowerCase() === romajiName.toLowerCase()) {
+        const userWasCorrect = userInput.toLowerCase() === romajiName.toLowerCase()
+        createUserStatusIcon(userWasCorrect);
+        if (userWasCorrect) {
           console.log('Great!');
-        } else {
+        } else if (!userWasCorrect && failures < 5) {
+          console.error('Try again!');
           failures++;
-          if (failures < 5) console.error('Try again!');
-          else console.log('Good try! It was:', romajiName);
+        } else {
+          console.log('Good try! It was:', romajiName);
+          failures = 0;
         }
+
       }
     })
-    .appendTo('.practice')
+    .appendTo('#user-input')
     .focus();
+}
+
+function createUserStatusIcon(status) {
+  const $status = $('#attempt-status');
+  const $icon = $('<i>').addClass('fas');
+  if (status) {
+    $icon.addClass('fa-thumbs-up');
+  } else {
+    $icon.addClass('fa-thumbs-down');
+  }
+  $status.empty().append($icon);
+}
+
+function createAttempt(type, katakana, romaji) {
+
 }
