6b9d123ee9c6957a68733edad9e96f894127562f
Olivia Sabo-Rush <w.phrush@gmail.com>
2018-04-23 23:29:21 -0500

    remove old code

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const colorDark = colors.hues[3];
 const colorLight = colors.hues[1];
 
 $(document).ready(function() {
-  const name = gimei.name();
-  const nameKatakana = name[alphabet]();
   $('body')
     .css({
       background: colorLight,
@@ -17,39 +15,12 @@ $(document).ready(function() {
     });
 
   createPracticeWord(alphabet, colorDark, colorLight);
-  // kuroshiro.init((err) => {
-  //   const splitName = nameKatakana.split('');
-  //   const nameRomaji = kuroshiro.convert(nameKatakana, { to: 'romaji' })
-  //   var romajiSyllables = splitName.map(kana => {
-  //     return kuroshiro.convert(kana, { to: 'romaji' });
-  //   });
-  //   $('.switch-alphabet').click(function(event) {
-  //     const $button = $(event.currentTarget);
-  //     const type = $button.attr('id');
-  //     alphabet = type;
-  //     $('.switch-alphabet').removeClass('active');
-  //     $(`#${type}`).addClass('active');
-  //     swapLanguages(colorLight, colorDark);
-  //     $('#type-alphabet').text(type.slice(0, 4).toUpperCase());
-  //   });
-  //   $('.spinner').toggle();
-  //   $('.top')
-  //     .css('background-color', colorDark)
-  //     .toggle()
-  //     .addClass('slide-open');
-  //   swapLanguages(colorLight, colorDark);
-  //   $('.bottom')
-  //     .css('color', colorDark)
-  //     .toggle();
-  //   createTextInput(colorDark, nameKatakana, nameRomaji);
-  // })
 });
 
 function createPracticeWord(type, colorDark, colorLight) {
   const gender = Math.round(Math.random()) ? 'male' : 'female';
   const name = gimei[gender]();
   const nameKatakana = name[type]();
-  console.log(nameKatakana);
   $('#practice-text')
     .text(nameKatakana);
   kuroshiro.init((err) => {
@@ -89,25 +60,22 @@ function createTextInput(color, nameKatakana, nameRomaji) {
       color: color,
       'border-bottom': `${color} 3px solid`,
     })
-    // .attr('placeholder', 'Practice Here')
     .keyup((event) => {
       const $event = $(event.currentTarget);
       const userInput = $event.val();
       if (event.keyCode === 13 && userInput) {
         const userWasCorrect = userInput.toLowerCase() === nameRomaji.toLowerCase()
         createUserStatusIcon(userWasCorrect);
+        $event.val('');
         if (userWasCorrect) {
-          $event.val('');
           console.log('Great!');
           createAttempt('success', nameKatakana, nameRomaji);
           createPracticeWord(alphabet, colorDark, colorLight);
         } else if (!userWasCorrect && failures < 4) {
-          $event.val('');
           console.error('Try again!');
           failures++;
           createAttempt('failure', nameKatakana, userInput);
         } else {
-          $event.val('');
           console.log('Good try! It was:', nameRomaji);
           createAttempt('failure', nameKatakana, userInput);
           createPracticeWord(alphabet, colorDark, colorLight);
